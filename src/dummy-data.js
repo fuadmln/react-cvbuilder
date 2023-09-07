c28cce06214893a9cfbce237ba3399701ea75401
Fuad Maulana <fuadm03@gmail.com>
2023-09-07 22:56:32 +0700

    feat: add dummy data for profile

---
+++ src/dummy-data.js
@@ -0,0 +1,9 @@
+const bio = {
+  name: 'Fuad Maulana',
+  email: 'whereismy@email.com',
+  phone: '+6281122334455',
+  address: 'P. Sherman, 42 Wallaby Way, Sydney',
+  about: 'Full Stack Developer is someone who not really good either as frontend or backend. On the frontend, they can\'t center a div. While being backend their code resembles a spaghetti factory that ready to blow the server. I\'m joking :)'
+}
+
+export default bio;
