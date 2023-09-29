e54480a1371a76936308ba16eb709af433f339f2
Fuad Maulana <fuadm03@gmail.com>
2023-09-30 03:25:15 +0700

    refactor: add id to array object

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -3,11 +3,12 @@ const bio = {
   email: 'whereismy@email.com',
   phone: '+6281122334455',
   address: 'P. Sherman, 42 Wallaby Way, Sydney',
-  about: 'Full Stack Developer is someone who not really good either as frontend or backend. On the frontend, they can\'t center a div. While being backend their code resembles a spaghetti factory that ready to blow the server. I\'m joking :)'
+  about: 'Full Stack Developer is someone who not really good either as frontend or backend. On the frontend, they can\'t center a div. While being backend their code resembles a spaghetti factory that ready to blow the server.'
 }
 
 const educations = [
   {
+    id: 2221,
     degree: 'school',
     institution: 'SMAN 1 Batam',
     major: 'Natural Science',
@@ -16,6 +17,7 @@ const educations = [
     location: 'Batam, Indonesia'
   },
   {
+    id: 2222,
     degree: 'bachelor',
     institution: 'Sriwijaya University',
     major: 'Informatics Engineering',
@@ -24,6 +26,7 @@ const educations = [
     location: 'Palembang, Indonesia'
   },
   {
+    id: 2223,
     degree: 'master',
     institution: 'Hogwarts University',
     major: 'Defence Against the Dark Arts',
@@ -35,6 +38,7 @@ const educations = [
 
 const experiences = [
   {
+    id: 3331,
     position: 'Frontend Developer',
     company: 'Facebook',
     yearIn: '2015',
@@ -42,6 +46,7 @@ const experiences = [
     description: 'Sit amet consectetur adipisicing elit. Eum, blanditiis. Qui, nisi? Ex commodi totam nulla expedita, ipsam deleniti voluptatum, autem unde magnam, voluptatibus sapiente et aspernatur suscipit tempora vitae lorem ipsum dolor s.'
   },
   {
+    id: 3332,
     position: 'Backend Developer',
     company: 'Tesla',
     yearIn: '2020',
