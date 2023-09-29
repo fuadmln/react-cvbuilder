129b3a9bc7fbcd83c7d6bd034bdf9b51feada042
Fuad Maulana <fuadm03@gmail.com>
2023-09-30 03:02:33 +0700

    refactor: convert to controlled component

--- src/components/FormPersonal.jsx
+++ src/components/FormPersonal.jsx
@@ -14,27 +14,27 @@ function PersonalForm({profile, setProfile}){
             <div>
                <label htmlFor="inputName">Full Name</label>
                <br />
-               <input type="text" id="inputName" name="name" placeholder="my name is .." onChange={changeProperty} />
+               <input type="text" id="inputName" name="name" placeholder="my name is .." onChange={changeProperty} value={profile.name} />
             </div>
             <div>
                <label htmlFor="inputPhone">Phone Number</label>
                <br />
-               <input type="tel" id="inputPhone" name="phone" placeholder="type your number" onChange={changeProperty} />
+               <input type="tel" id="inputPhone" name="phone" placeholder="type your number" onChange={changeProperty} value={profile.phone} />
             </div>
             <div>
                <label htmlFor="inputEmail">Email</label>
                <br />
-               <input type="email" id="inputEmail" name="email" placeholder="type your email" onChange={changeProperty} />
+               <input type="email" id="inputEmail" name="email" placeholder="type your email" onChange={changeProperty} value={profile.email} />
             </div>
             <div>
                <label htmlFor="inputAddress">Address</label>
                <br />
-               <input type="text" id="inputAddress" name="address" placeholder="I live at .." onChange={changeProperty} />
+               <input type="text" id="inputAddress" name="address" placeholder="I live at .." onChange={changeProperty} value={profile.address} />
             </div>
             <div>
                <label htmlFor="inputAbout">About</label>
                <br />
-               <textarea id="inputAbout" cols="30" rows="10" name="about" placeholder="describe your self" onChange={changeProperty}></textarea>
+               <textarea id="inputAbout" cols="30" rows="10" name="about" placeholder="describe your self" onChange={changeProperty} value={profile.about}></textarea>
             </div>
          </form>
       </div>
