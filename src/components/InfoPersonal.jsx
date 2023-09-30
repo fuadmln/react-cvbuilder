402d32c9d7e571cc804f1c1473c2b371d0e302a1
Fuad Maulana <fuadm03@gmail.com>
2023-09-30 21:59:21 +0700

    styling: personal info

--- src/components/InfoPersonal.jsx
+++ src/components/InfoPersonal.jsx
@@ -4,12 +4,40 @@ function InfoPersonal({profile}){
    return(
       <div className="cv-personal">
          <h2>{profile.name}</h2>
-         <p>{profile.about}</p>
          <ul>
-            <li><b>Phone: </b>{profile.phone}</li>
-            <li><b>Email: </b>{profile.email}</li>
-            <li><b>Address: </b>{profile.address}</li>
+            {profile.phone && (
+               <li>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#888" class="bi bi-telephone-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/></svg>
+                  <span>{profile.phone}</span>
+               </li>
+            )}
+            {profile.email && (
+               <li>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#888" class="bi bi-envelope-fill" viewBox="0 0 16 16"><path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/></svg>
+                  <span>{profile.email}</span>
+               </li>
+            )}
+            {profile.address && (
+               <li>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#888" class="bi bi-geo-alt-fill" viewBox="0 0 16 16"><path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/></svg>
+                  <span>{profile.address}</span>
+               </li>
+            )}
+            {profile.link && (
+               <li>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#888" class="bi bi-link-45deg" viewBox="0 0 16 16"><path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"/><path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"/></svg>
+                  <span>https://google.com</span>
+               </li>
+            )}
+            {profile.linkedin && (
+               <li>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#888" class="bi bi-linkedin" viewBox="0 0 16 16"><path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/></svg>
+                  <span>linkedin.com</span>
+               </li>
+            )}
          </ul>
+         <p>{profile.about}</p>
+         
       </div>
    );
 }
